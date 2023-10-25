6a030b166d55ddc7e49ebe5e80cb94cdcf04127c
Ahmed Fayez <ahmed.fayez.ayesh@gmail.com>
2023-10-25 07:20:28 +0300

    Custom bg Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            backgroundColor:{
+                'todo-gray': '#f7f8fc',
+            }
         },
     },
     variants: {
